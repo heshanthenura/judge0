7e2137bbfef97e15d055e3d891d5e2263aa22976
IT24100028 <heshanthenura@protonmail.com>
2025-12-06 17:38:15 +0530

    fix vercel error

--- app/api/questions/[id]/route.ts
+++ app/api/questions/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  context: { params: { id: string } }
+  context: { params: { id: string } | Promise<{ id: string }> }
 ) {
   try {
     const { id } = await context.params;
